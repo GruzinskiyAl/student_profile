8a8116a5611b2bb7ee3664279bdc0078c968adb0
GruzinskiyAl <gruzinskiy.al@gmail.com>
2018-06-21 11:35:21 +0300

    decorator

--- apps/subject_controller/forms.py
+++ apps/subject_controller/forms.py
@@ -1,8 +1,13 @@
 from django.forms import ModelForm
+from django import forms
 from apps.main.models import Student
 
 
 class ImageForm(ModelForm):
+    # t_number = forms.CharField(max_length=10)
+    # email = forms.EmailField
+    # photo = forms.ImageField
+
     class Meta:
         model = Student
         fields = ['t_number', 'email', 'photo']
--- apps/subject_controller/views.py
+++ apps/subject_controller/views.py
@@ -16,92 +16,62 @@ from .to_dict_functions import (schedule_for_template,
                                 subject_list_with_marks,
                                 chart_marks_data)
 from .forms import ImageForm
+from .decorators import auth_check
 
 
 class SubjectList(View):
+
+    @auth_check
     def get(self, request):
-        if request.user.is_authenticated:
-            try:
-                student = Student.objects.get(user=request.user)
-                subjects_dict = subject_list_with_marks(student)
-                return render(
-                    request, 'subject_list.html', {
-                        'subjects_with_mark': subjects_dict,
-                        'student': student})
-            except Student.DoesNotExist:
-                try:
-                    Teacher.objects.get(user=request.user)
-                except Teacher.DoesNotExist:
-                    return redirect('/admin/')
-        else:
-            return redirect('/login/')
+        student = Student.objects.get(user=request.user)
+        subjects_dict = subject_list_with_marks(student)
+        return render(
+            request, 'subject_list.html', {
+                'subjects_with_mark': subjects_dict,
+                'student': student})
 
 
 class StudentSchedule(View):
 
+    @auth_check
     def get(self, request):
-        if request.user.is_authenticated:
-            try:
-                student = Student.objects.get(user=request.user)
-                schedules_queryset = WeekSchedule.objects.filter(
-                    univ_group=student.univ_group)
-                return render(
-                    request, 'schedule.html', {
-                        'day_list': schedule_for_template(schedules_queryset),
-                        'student': student})
-            except Student.DoesNotExist:
-                try:
-                    Teacher.objects.get(user=request.user)
-                except Teacher.DoesNotExist:
-                    return redirect('/admin/')
-        else:
-            return redirect('/login/')
+        student = Student.objects.get(user=request.user)
+        schedules_queryset = WeekSchedule.objects.filter(
+            univ_group=student.univ_group)
+        return render(
+            request, 'schedule.html', {
+                'day_list': schedule_for_template(schedules_queryset),
+                'student': student})
 
 
 class GroupExamList(View):
 
+    @auth_check
     def get(self, request):
-        if request.user.is_authenticated:
-            try:
-                student = Student.objects.get(user=request.user)
-                exam_list = GroupExam.objects.filter(
-                    univ_group=student.univ_group, exam_datetime__gt=datetime.now())
-                return render(
-                    request, 'exam_list.html', {
-                        'student': student, 'exam_list': exam_list})
-            except Student.DoesNotExist:
-                try:
-                    Teacher.objects.get(user=request.user)
-                except Teacher.DoesNotExist:
-                    return redirect('/admin/')
-        else:
-            return redirect('/login/')
+        student = Student.objects.get(user=request.user)
+        exam_list = GroupExam.objects.filter(
+            univ_group=student.univ_group, exam_datetime__gt=datetime.now())
+        return render(
+            request, 'exam_list.html', {
+                'student': student, 'exam_list': exam_list})
 
 
 class SubjectMaterial(View):
 
+    @auth_check
     def get(self, request, subject_pk):
-        if request.user.is_authenticated:
-            try:
-                student = Student.objects.get(user=request.user)
-                subject = Subject.objects.get(pk=subject_pk)
-                labs, lectures, literature_list, materials = subject_materials_for_template(
-                    subject)
-                return render(request,
-                              'subject_material.html',
-                              {'labs': labs,
-                               'lectures': lectures,
-                               'literature_list': literature_list,
-                               'materials': materials,
-                               'student': student,
-                               'subject': subject})
-            except Student.DoesNotExist:
-                try:
-                    Teacher.objects.get(user=request.user)
-                except Teacher.DoesNotExist:
-                    return redirect('/admin/')
-        else:
-            return redirect('/login/')
+        student = Student.objects.get(user=request.user)
+        subject = Subject.objects.get(pk=subject_pk)
+        labs, lectures, literature_list, materials = subject_materials_for_template(
+            subject)
+        return render(request,
+                      'subject_material.html',
+                      {'labs': labs,
+                       'lectures': lectures,
+                       'literature_list': literature_list,
+                       'materials': materials,
+                       'student': student,
+                       'subject': subject})
 
 
 def update_image(request):
@@ -110,6 +80,7 @@ def update_image(request):
             form = ImageForm(request.POST, request.FILES)
             user = request.user
             student = Student.objects.get(user=user)
+            print(form.errors)
             if form.is_valid():
                 student.photo = request.FILES['photo']
                 student.t_number = request.POST['t_number']
@@ -125,77 +96,45 @@ def update_image(request):
 
 class StudentMarkBySubject(View):
 
+    @auth_check
     def get(self, request, subject_pk):
-        if request.user.is_authenticated:
-            try:
-                student = Student.objects.get(user=request.user)
-                subject = SubjectOfUnivGroup.objects.get(pk=subject_pk)
-                marks = StudentSubjectMark.objects.filter(
-                    student=student, subject=subject)
-                return render(
-                    request, 'subject_marks.html', {
-                        'student': student, 'marks': marks, 'subject': subject})
-            except Student.DoesNotExist:
-                try:
-                    Teacher.objects.get(user=request.user)
-                except Teacher.DoesNotExist:
-                    return redirect('/admin/')
-        else:
-            return redirect('/login/')
+        student = Student.objects.get(user=request.user)
+        subject = SubjectOfUnivGroup.objects.get(pk=subject_pk)
+        marks = StudentSubjectMark.objects.filter(
+            student=student, subject=subject)
+        return render(
+            request, 'subject_marks.html', {
+                'student': student, 'marks': marks, 'subject': subject})
 
 
 class StudentMarksChart(View):
 
+    @auth_check
     def get(self, request):
-        if request.user.is_authenticated:
-            try:
-                student = Student.objects.get(user=request.user)
-                return render(
-                    request, 'marks_chart.html', {
-                        'student': student, 'values': chart_marks_data(student)})
-            except Student.DoesNotExist:
-                try:
-                    Teacher.objects.get(user=request.user)
-                except Teacher.DoesNotExist:
-                    return redirect('/admin/')
-        else:
-            return redirect('/login/')
+        student = Student.objects.get(user=request.user)
+        return render(
+            request, 'marks_chart.html', {
+                'student': student, 'values': chart_marks_data(student)})
 
 
 class TeacherMessages(View):
 
+    @auth_check
     def get(self, request):
-        if request.user.is_authenticated:
-            try:
-                student = Student.objects.get(user=request.user)
-                messages = TeacherMessage.objects.filter(
-                    univ_group=student.univ_group)
-                return render(
-                    request, 'teachers_messages.html', {
-                        'student': student, 'messages': messages})
-            except Student.DoesNotExist:
-                try:
-                    Teacher.objects.get(user=request.user)
-                except Teacher.DoesNotExist:
-                    return redirect('/admin/')
-        else:
-            return redirect('/login/')
+        student = Student.objects.get(user=request.user)
+        messages = TeacherMessage.objects.filter(
+            univ_group=student.univ_group)
+        return render(
+            request, 'teachers_messages.html', {
+                'student': student, 'messages': messages})
 
 
 class TeachersList(View):
 
+    @auth_check
     def get(self, request):
-        if request.user.is_authenticated:
-            try:
-                student = Student.objects.get(user=request.user)
-                teachers = Teacher.objects.all()
-                return render(
-                    request, 'teachers_list.html',
-                    {'student': student, 'teachers': teachers})
-            except Student.DoesNotExist:
-                try:
-                    Teacher.objects.get(user=request.user)
-                except Teacher.DoesNotExist:
-                    return redirect('/admin/')
-        else:
-            return redirect('/login/')
+        student = Student.objects.get(user=request.user)
+        teachers = Teacher.objects.all()
+        return render(
+            request, 'teachers_list.html',
+            {'student': student, 'teachers': teachers})
